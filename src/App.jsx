cf928601eaed50dbff75ef5c7807c6dce12c0509
Aznviben <72636205+Aznviben@users.noreply.github.com>
2022-10-19 18:39:23 -0400

    Added Components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+//Components
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+import Nav from './components/nav/nav'
+
+//Pages
+import About from './components/about/about'
+import Contacts from './components/contacts/contact'
+import Experience from './components/experience/experience'
+import Portfolio from './components/portfolio/portfolio'
+function App() {
+  return (
+    <>
+
+
+    </>
+  )
+}
+
+export default App
