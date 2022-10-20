99bb3fb0a0a78b6adf4ef9e6fcad65b3f5b18749
Aznviben <72636205+Aznviben@users.noreply.github.com>
2022-10-20 16:48:04 -0400

    Added a background

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import Background from './Background.jsx'
 const Header = () => {
   return (
     <section id="Header">
-      Header
+      <h3>Hi im,</h3>
+      <h1>Benjiman Dinal</h1>
+    <div className= 'container Header_container'> 
+    <Background/>
+    </div>
+
     </section>
   )
 }
