9dce1e7c74276ce1efc9a3a362a5ac02e1bbe8ff
Aznviben <72636205+Aznviben@users.noreply.github.com>
2022-10-20 16:25:26 -0400

    Added Nav.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import "./Nav.scss"
+import {AiFillHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {MdRateReview} from 'react-icons/md'
+import{useState} from 'react'
 const Nav = () => {
+  const [activeNav,setActiveNav]= useState('#'); 
   return (
     <section id="Nav">
-      Nav
+      <nav>
+        <a href = "#"onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiFillHome/></a>
+        <a href = "#About" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>        
+        <a href = "#Contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdRateReview/></a>
+      </nav>
     </section>
   )
 }
