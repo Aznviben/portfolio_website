81262032418c8eb6a2eafcd31defdfa4cc02c8a5
Aznviben <72636205+Aznviben@users.noreply.github.com>
2022-11-29 12:11:02 -0500

    about changes

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import './About.scss'
+import {BsFillAwardFill} from 'react-icons/bs'
+import {FiUsers} from 'react-icons/fi'
+import {AiFillFileText} from 'react-icons/ai'
 const About = () => {
   return (
     <section id = "about">
-      <div className= "container about-container">
-        
-      </div>
-        About
+          <h2>About Me</h2>
+
+          <div className="container about__container">
+            <div className="about__me">
+              <div className="about__me-image">
+              </div>
+            </div>
+              
+            <div className="about__content">
+            <div className="about__cards">
+              <article className="about__card">
+              <BsFillAwardFill className='about__icon'/>
+              <h5> Experience </h5>
+              <small>4+ Years of Programming</small>
+              </article>
+              <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5> Work </h5>
+              <small>1+ year of work experience</small>
+              </article>
+              <article className="about__card">
+              <AiFillFileText className='about__icon'/>
+              <h5>Involved</h5>
+              <small>Student Gov. Presidential Ambassador</small>
+              </article>
+            </div>
+            <p>
+ 
+            </p>
+            <a href='#contact' className='btn btn-primary'>Lets Talk</a>
+
+            </div>
+          </div>
     </section>
 
   )
--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+function sendEmail() 
+{
+    window.location = "mailto:benjamin.dinal@gmail.com";
+}
 const Contacts = () => {
   return (
     <section id="contact">
-        Contacts
+        <h1>Contacts</h1>
+    <div className = "number">
+    352 603 4770
+
+    </div>
+    <div className = "email" onclick="sendEmail();">
+    <p onclick="sendEmail();">benjamin.dinal@gmail.com </p>
+    </div>
+
+
+
     </section>
   )
 }
